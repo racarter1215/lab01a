8261e580d23c9a9f461a5f722a2fca550a95623a
racarter1215 <racarter1215@gmail.com>
2020-07-07 07:23:19 -0700

    the actual lab

---
+++ GetStartedDebugging/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GetStartedDebugging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
